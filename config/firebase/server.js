4de0fe3cfc53d0622affa82066af33612b7cd434
Bruno Bertolini <dev@brunobertolini.com>
2021-01-30 16:35:29 -0300

    feat: setup firebase admin

---
+++ config/firebase/server.js
@@ -0,0 +1,20 @@
+import admin from 'firebase-admin'
+
+const app = firebase.apps.length
+    ? firebase.app()
+    : firebase.initializeApp({
+        credential: admin.credential.cert({
+            type: "service_account",
+            auth_uri: "https://accounts.google.com/o/oauth2/auth",
+            token_uri: "https://oauth2.googleapis.com/token",
+            auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+            project_id: process.env.PROJECT_ID,
+            private_key_id: process.env.PRIVATE_KEY_ID,
+            private_key: process.env.PRIVATE_KEY,
+            client_email: process.env.CLIENT_EMAIL,
+            client_id: process.env.CLIENT_ID,
+            client_cert: process.env.CLIENT_CERT
+        })
+    })
+
+export default admin
