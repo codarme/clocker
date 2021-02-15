e915d0903985c3e282ec0f676ebb27aaa648a209
Bruno Bertolini <dev@brunobertolini.com>
2021-02-15 17:54:52 -0300

    fix: scape new line from firebase private key

--- config/firebase/server.js
+++ config/firebase/server.js
@@ -10,7 +10,7 @@ const app = firebaseServer.apps.length
             auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
             project_id: process.env.PROJECT_ID,
             private_key_id: process.env.PRIVATE_KEY_ID,
-            private_key: process.env.PRIVATE_KEY,
+            private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
             client_email: process.env.CLIENT_EMAIL,
             client_id: process.env.CLIENT_ID,
             client_cert: process.env.CLIENT_CERT
